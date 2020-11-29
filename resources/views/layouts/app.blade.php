255a36aa2dc563d5332378d0d81e55ff1959c52b
Sofen <sofen@xobotronix.com>
2020-11-29 11:44:30 +0600

    Check

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,19 +3,15 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
         <link rel="shortcut icon" type="image/x-icon" href="{{asset('public/assets/images/epc_favicon.png')}}" alt="logo 0">
         <title>EPC-ERP</title>
         <!-- Scripts -->
         <script src="{{ asset('public/js/app.js') }}" defer></script>
-
         <!-- Fonts -->
         <link rel="dns-prefetch" href="//fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
-
         <!-- Styles -->
         <link href="{{ asset('public/css/app.css') }}" rel="stylesheet">
     </head>
